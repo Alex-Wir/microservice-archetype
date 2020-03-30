2f71d3637a09eb253a578ed9162fb5ebb5b368c2
Alex Apon <nemanwir@gmail.com>
2020-03-30 12:23:42 +0300

    Add generating script

---
+++ generate.sh
@@ -0,0 +1,8 @@
+mvn archetype:generate -B\
+  -DarchetypeCatalog=local\
+  -DarchetypeGroupId=com.gmail.rec0rd.alterego\
+  -DarchetypeArtifactId=microservice-archetype\
+  -DgroupId=com.group\
+  -DartifactId=servicename\
+  -Dversion=1.0\
+  -DoutputDirectory=../
